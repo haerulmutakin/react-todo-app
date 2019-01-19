287a436ed0f842df97d690a076f873f4eb82b791
Haerul Mutakin <haerul.mutakin@uii.ac.id>
2019-01-19 22:45:45 +0700

    Updates

---
+++ precache-manifest.f6b21dc9674cfa7993df27bc3c7c693f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de34005e7bf5a729e541",
+    "url": "/static/js/main.de34005e.chunk.js"
+  },
+  {
+    "revision": "95cd5bb197670939350b",
+    "url": "/static/js/1.95cd5bb1.chunk.js"
+  },
+  {
+    "revision": "de34005e7bf5a729e541",
+    "url": "/static/css/main.4f911549.chunk.css"
+  },
+  {
+    "revision": "375153753b1791dbe1028eab2e01cf3a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-todo-app/precache-manifest.bd6c5245ac23c6c89dec7949e04c590a.js"
+  "/precache-manifest.f6b21dc9674cfa7993df27bc3c7c693f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-todo-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
