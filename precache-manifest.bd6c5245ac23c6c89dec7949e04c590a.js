bfd00a26b222f7918f5225a4588f0ec1dab060a3
Haerul Mutakin <haerul.mutakin@uii.ac.id>
2019-01-19 22:38:48 +0700

    Updates

---
+++ precache-manifest.bd6c5245ac23c6c89dec7949e04c590a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dc884f14d85a35ea3dce",
+    "url": "/react-todo-app/static/js/runtime~main.dc884f14.js"
+  },
+  {
+    "revision": "8f1f2dccdc3cc27a17c5",
+    "url": "/react-todo-app/static/js/main.8f1f2dcc.chunk.js"
+  },
+  {
+    "revision": "95cd5bb197670939350b",
+    "url": "/react-todo-app/static/js/1.95cd5bb1.chunk.js"
+  },
+  {
+    "revision": "8f1f2dccdc3cc27a17c5",
+    "url": "/react-todo-app/static/css/main.4f911549.chunk.css"
+  },
+  {
+    "revision": "cced87b8579dc47d95842eb87a3377b5",
+    "url": "/react-todo-app/index.html"
+  }
+];
